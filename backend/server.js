feb2242b65d97e692281f48425b62ff673e59a1a
kiashakiarad <saycho@gmail.com>
2026-06-28 21:54:51 +0330

    fix render connection reset

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send("OK");
-});
-
 app.get("/ping", (req, res) => {
   res.status(200).send("pong");
 });
 
 app.post("/chat", (req, res) => {
-  res.json({ reply: "test" });
+  res.json({ reply: "ok" });
 });
 
 const PORT = process.env.PORT || 10000;
